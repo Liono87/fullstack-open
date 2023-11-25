15ea5dd7bf2431a4a30d8213fadb1054fc7a3cec
Liono87 <glahiru10@gmail.com>
2023-11-25 18:31:04 +0530

    complete 2.12 the phonebook step7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -23,6 +23,32 @@ const App = () => {
       });
   }, []);
 
+  /* 
+  
+  useEffect(() => {
+  console.log('effect');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/persons');
+      const data = response.data;
+
+      for (let i = 0; i < data.length; i++) {
+        setTimeout(() => {
+          setPersons((prevPersons) => [...prevPersons, data[i]]);
+        }, (i + 1) * 2000); // 2-second delay between each item
+      }
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  };
+
+  fetchData();
+}, []);
+
+  
+  */
+
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
@@ -35,33 +61,53 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const addPerson = (event) => {
+  const addPerson = async (event) => {
     event.preventDefault(); // Prevent default form submission
 
-    const nameExists = persons.some((person) => person.name === newName);
+    try {
+      const nameExists = persons.some((person) => person.name === newName);
 
-    if (nameExists) {
-      const updatedPersons = persons.map((person) =>
-        person.name === newName ? { ...person, number: newNumber } : person,
-      );
-      setPersons(updatedPersons);
+      if (nameExists) {
+        // Update existing person's number
+        const updatedPersons = persons.map((person) =>
+          person.name === newName ? { ...person, number: newNumber } : person,
+        );
+        setPersons(updatedPersons);
+      } else {
+        // Save a new person
+        await savePerson(newName, newNumber);
+        const personObject = {
+          name: newName,
+          number: newNumber,
+          id: persons.length + 1,
+        };
+        setPersons([...persons, personObject]);
+      }
+
       setNewName('');
       setNewNumber('');
-    } else {
-      const personObject = {
-        name: newName,
-        number: newNumber,
-        id: persons.length + 1,
-      };
-      setPersons([...persons, personObject]);
-      setNewName('');
-      setNewNumber('');
+    } catch (error) {
+      console.log(error.message);
     }
   };
 
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(searchTerm.toLowerCase()),
   );
+
+  // Create a function to save data
+  const savePerson = async (name, number) => {
+    try {
+      const response = await axios.post('http://localhost:3001/persons', {
+        name: name,
+        number: number,
+      });
+      return response.data; // Optionally return data after saving
+    } catch (error) {
+      console.log('Error saving person: ', error);
+      throw error;
+    }
+  };
 
   return (
     <div>
